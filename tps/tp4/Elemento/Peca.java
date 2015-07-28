4aabb9c32317a6747e31463d6aefd71cdc168391
verahgfreire <verafreire@MacBook-Pro-de-Vera.local>
2015-07-28 02:00:02 +0100

    circulo a volta da peca
    falta o stroke

--- tps/tp4/Elemento/Peca.java
+++ tps/tp4/Elemento/Peca.java
@@ -3,8 +3,10 @@ package tps.tp4.Elemento;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.geom.*;
 
 import javax.swing.border.LineBorder;
+import javax.swing.SwingConstants;
 
 public class Peca extends Elemento{
 
@@ -16,11 +18,14 @@ public class Peca extends Elemento{
 		setText("" + num);
 		setForeground(Color.WHITE);
 		setFont(new Font("Courier New", Font.BOLD + Font.ITALIC, 40));
-
+		setHorizontalAlignment(SwingConstants.CENTER);
 	}
 
 	@Override
     protected void paintComponent (Graphics g){
-        super.paintComponent(g); 
+        super.paintComponent(g);
+        g.setColor(Color.YELLOW);
+        g.drawOval(getWidth()/2 - 40, getHeight()/2 -40, 80, 80);
+        //g.setStroke(10);
 	}
 }
