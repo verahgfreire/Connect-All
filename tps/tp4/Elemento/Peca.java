a8e1f758d6c22e37c915268267901449e64d6339
verahgfreire <verafreire@MacBook-Pro-de-Vera.local>
2015-07-28 15:25:44 +0100

    Peca com stroke

--- tps/tp4/Elemento/Peca.java
+++ tps/tp4/Elemento/Peca.java
@@ -3,7 +3,6 @@ package tps.tp4.Elemento;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.geom.*;
 
 import javax.swing.border.LineBorder;
 import javax.swing.SwingConstants;
@@ -15,17 +14,18 @@ public class Peca extends Elemento{
 	public Peca(int num){
 		super();
 		this.num = num;
-		setText("" + num);
-		setForeground(Color.WHITE);
-		setFont(new Font("Courier New", Font.BOLD + Font.ITALIC, 40));
-		setHorizontalAlignment(SwingConstants.CENTER);
 	}
 
 	@Override
     protected void paintComponent (Graphics g){
         super.paintComponent(g);
         g.setColor(Color.YELLOW);
-        g.drawOval(getWidth()/2 - 40, getHeight()/2 -40, 80, 80);
-        //g.setStroke(10);
+        //g.drawOval(getWidth()/2 - 40, getHeight()/2 -40, 80, 80);
+        g.fillOval(getWidth()/2 - 40, getHeight()/2 -40, 80, 80);
+        g.setColor(Color.BLACK);
+        g.fillOval(getWidth()/2 - 25, getHeight()/2 -25, 50, 50);
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Courier New", Font.BOLD + Font.ITALIC, 40));
+        g.drawString("" + num, getWidth()/2-13, getHeight()/2+12);
 	}
 }
